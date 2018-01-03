aa5dff38f97835084de25be79e977fb0530f4773
ASharikov <alexsander.sharikov@yandex.ru>
2018-01-04 01:35:09 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,12 @@
 package ru.job4j;
-
+/**
+ * Calculate. 
+ */
 public class Calculate {
+	/**
+	 * Конструктор, вывод строки в консоль.
+	 * @param args - args
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
