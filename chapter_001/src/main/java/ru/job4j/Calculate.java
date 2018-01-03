703f462745b96d94f18db247261ec6f176c3843d
ASharikov <alexsander.sharikov@yandex.ru>
2018-01-04 02:05:51 +0300

    package ru.job4j;

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,9 @@
 package ru.job4j;
 /**
- * Calculate. 
+ * Calculate.
+ * @author Alexander Sharikov (alexsander.sharikov@yandex.ru)
+ * @version $Id$
+ * @since 0.1
  */
 public class Calculate {
 	/**
@@ -9,5 +12,13 @@ public class Calculate {
 	 */
 	public static void main(String[] args){
 		System.out.println("Hello world");
+	}
+	/**
+	 *Method echo.
+	 *@param name Your name.
+	 *@return Echo plus your name.
+	 */
+	public String echo(String name){
+		return "Echo, echo, echo: " + name;
 	}
 }
