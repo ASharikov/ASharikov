9abc487893a641b1a30a064b3db09476358f389d
ASharikov <alexsander.sharikov@yandex.ru>
2018-01-11 15:08:03 +0300

    Работа над ошибками.

---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+
+public class TriangleTest {
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        // создаем три объекта класс Point.
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 0);
+
+        // создаем объетк треугольник и передаем внего объекты точек.
+        Triangle triangle = new Triangle(a, b, c);
+        // вычисляем площадь.
+        double result = triangle.area();
+        // Задаем ожидаемый результат.
+        double expected = 2D;
+        // проверяем результат и ожидаемое значение.
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Alexander Sharikov (alexsander.sharikov@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
