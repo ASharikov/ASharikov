5816f3c1cb2fca12f4f040c0035bc8d0284b4325
ASharikov <alexsander.sharikov@yandex.ru>
2018-01-11 16:07:43 +0300

    Работа над ошибками v4

--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -11,7 +11,7 @@ public class FactorialTest {
         Factorial factorial = new Factorial();
         int result = factorial.calc(5);
         int expected = 120;
-        assertThat(result,is(expected));
+        assertThat(result, is(expected));
     }
 
     @Test
@@ -19,6 +19,6 @@ public class FactorialTest {
         Factorial factorial = new Factorial();
         int result = factorial.calc(0);
         int expected = 1;
-        assertThat(result,is(expected));
+        assertThat(result, is(expected));
     }
 }
