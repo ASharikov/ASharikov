2b773c4612c6dba21f6a60c1eda8c46628ba78ed
ASharikov <alexsander.sharikov@yandex.ru>
2018-01-11 15:08:48 +0300

    Работа над ошибками.

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -16,9 +16,9 @@ public class Board {
     public String paint(int width, int height) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-        for(int i = 0; i < height; i++) {
-            for(int j = 0; j < width; j++) {
-                if((j % 2 == 0) & (i % 2 == 0) | ((j % 2 != 0) & (i % 2 != 0))) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if ((j + i) % 2 == 0) {
                     screen.append("x");
                 } else {
                     screen.append(" ");
